5572a2d1ac129b20ed43415c2545526b45488907
Breanna Patz <39364807-pipsnchicks@users.noreply.replit.com>
2025-03-05 18:24:06 +0000

    Restored to 'daab44f92289096768b6c9e289a0147567d0f90f'
    Replit-Restored-To: daab44f92289096768b6c9e289a0147567d0f90f

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,21 +4,6 @@ export default {
   darkMode: ["class"],
   content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    screens: {
-      'xs': '480px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
     extend: {
       borderRadius: {
         lg: "var(--radius)",
